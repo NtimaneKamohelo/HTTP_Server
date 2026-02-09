1830be69e9c5a04cf7011a5202f2981179af98a7
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2026-02-09 13:50:53 +0200

    Added a read me

---
+++ httpServer/src/main/java/org/example/httpServer/HttpServer.java
@@ -0,0 +1,15 @@
+package org.example.httpServer;
+
+/**
+ *
+ * Driver class for HTTP Server
+ *
+ */
+
+public class HttpServer {
+
+    public static void main(String[] args){
+        System.out.println("Server Starting...");
+    }
+
+}
